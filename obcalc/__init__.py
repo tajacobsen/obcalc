f1cf111cd3c8b62646ca5a29e615b534f8a9c35c
Troels Kofoed Jacobsen <tkjacobsen@gmail.com>
2010-03-15 12:28:01 +0100

    Output information to text file (especiall the bond information is valuable)

--- obcalc/__init__.py
+++ obcalc/__init__.py
@@ -9,6 +9,8 @@ O'Boyle et al., Chem. Cent. J., 2, 5 (2008), doi:10.1186/1752-153X-2-5
 Copyright (C) 2010 by Troels Kofoed Jacobsen
 Code released under GPLv2. See COPYING for details.
 """
+import sys
+import time
 
 import numpy as np
 import openbabel as ob
@@ -29,7 +31,7 @@ class OBForceField:
         http://www.uku.fi/~thassine/projects/ghemical/
 
     """
-    def __init__(self, force_field='UFF', bonds=None):
+    def __init__(self, force_field='UFF', bonds=None, txt=None):
         """Construct OpenBabel Force Field Calculator object.
 
         Parameters
@@ -43,6 +45,8 @@ class OBForceField:
                 ]
             If None the calculator will try to construct the bonds
             automatically.
+        txt: str
+            Which file to write output too. Defaults to stdout ('-').
         """
 
         self.force_field = force_field
@@ -51,6 +55,13 @@ class OBForceField:
 
         self.energy = None
         self.force = None
+
+        if txt is None:
+            self.txt = None
+        elif txt == '-':
+            self.txt = sys.stdout
+        else:
+            self.txt = open(txt, 'w')
 
     def get_potential_energy(self, atoms=None, force_consistent=False):
         """Return total energy"""
@@ -108,3 +119,22 @@ class OBForceField:
             self.forces = forces
 
             self.atoms = atoms.copy()
+
+            # Print output
+            if self.txt is not None:
+                print >> self.txt, time.asctime()
+
+                obConversion = ob.OBConversion()
+                obConversion.SetInAndOutFormats("mol", "molreport")
+                out = obConversion.WriteString(mol)
+                print >> self.txt,  out
+
+                print >> self.txt, 'Energy in eV:\t%.4f' % self.energy
+                print >> self.txt, ''
+                symbols = self.atoms.get_chemical_symbols()
+                print >> self.txt, 'Forces in eV/Ang:'
+                for a, symbol in enumerate(symbols):
+                    print >> self.txt, '%3d %-2s %10.5f %10.5f %10.5f' % \
+                        ((a, symbol) + tuple(self.forces[a]))
+                print >> self.txt, '\n'
+
