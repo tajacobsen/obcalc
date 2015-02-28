1651cb76c3a41e7126c623a581e183a9d48e315b
Troels Kofoed Jacobsen <tkjacobsen@gmail.com>
2015-02-28 07:42:42 +0100

    Adding example from README as separate files

---
+++ examples/Azobenzene.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python2
+from obcalc.tools import build_molecule
+atoms = build_molecule('C1=CC=CC=C1N=NC2=CC=CC=C2')
---
+++ examples/CO2-relax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+from ase import Atoms, QuasiNewton
+from obcalc import OBForceField
+
+atoms = Atoms('OCO', [[0.0, 0.0, 0.0],
+                      [3.0, 0.0, 0.0],
+                      [6.0, 0.0, 0.0]])
+
+calc = OBForceField()
+atoms.set_calculator(calc)
+relax = QuasiNewton(atoms)
+relax.run(fmax=0.05)
+# This results in a even larger interatomic distances
+# Therefore we add two double bonds
+
+bonds = [[0, 1, 2], # Double bond between atom 0 and atom 1
+         [1, 2, 2]] # Double bond between atom 1 and atom 2
+calc = OBForceField(bonds=bonds)
+atoms.set_calculator(calc)
+relax = QuasiNewton(atoms)
+relax.run(fmax=0.05)
+# This gives the expected results
